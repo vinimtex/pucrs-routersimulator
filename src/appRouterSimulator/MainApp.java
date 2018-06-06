1e4c391f6efdfe7857c2d134f30477740809c566
João Paulo Ferreira França <joao.franca@acad.pucrs.br>
2018-06-06 00:34:56 -0300

    added method to read .txt of ip's

--- src/appRouterSimulator/MainApp.java
+++ src/appRouterSimulator/MainApp.java
@@ -1,17 +1,25 @@
 package appRouterSimulator;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.nio.file.Files;
 import java.util.*;
 
 import services.RouterService;
 
 public class MainApp {
 	
+	public final static String PathIPList = "C:\\Users\\João França\\Desktop\\pucrs-routersimulator\\src\\appRouterSimulator\\IPList.txt";
+	
 	public static List<String> ipList = new ArrayList<>();
 	
 	public static void main(String[] args) {
-		ipList.add("192.168.1.109");
+		
+		ReadIPList();
 		
 		try {
 			RouterService.startRouter(InetAddress.getLocalHost().getHostAddress());
@@ -22,4 +30,23 @@ public class MainApp {
 		
 	}
 
+	public static void ReadIPList() {
+		
+		try {
+
+ 		    FileReader fileReader = new FileReader(PathIPList);
+
+		    try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+		      String IP;
+		      while((IP = bufferedReader.readLine()) != null) {
+		        ipList.add(IP);
+		      };
+		      
+		    }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
 }
